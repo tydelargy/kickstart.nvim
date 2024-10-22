b8e1b5f5e04844d5685a706f0ff4a81549174daf
tycodeguy <tjdelargy@gmail.com>
2024-10-22 02:48:45 -0400

    Create rust_wasm.lua

---
+++ lua/custom/plugins/rust_wasm.lua
@@ -0,0 +1,22 @@
+do
+  return {}
+end
+
+-- require('lspconfig').rust_analyzer.setup {
+--   -- Other Configs ...
+--   settings = {
+--     ["rust-analyzer"] = {
+--       -- Other Settings ...
+--       procMacro = {
+--         ignored = {
+--             leptos_macro = {
+--                 -- optional: --
+--                 -- "component",
+--                 "server",
+--             },
+--         },
+--       },
+--     },
+--   }
+-- }
+--
